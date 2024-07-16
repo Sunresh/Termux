9f2b39f67debc1bf556a5a465887ab8673d66b8b
Naresh Dhungana <hinareshg@gmail.com>
2024-07-16 11:01:43 +0900

    uu

--- platform.sh
+++ platform.sh
@@ -17,13 +17,18 @@ function linux() {
   echo "To copy text to clipboard: "
   echo "  echo \"Your text here\" | xclip"
 }
-
+function echo_text() {
+  echo "Usage: platform.sh"
+  echo "This script provides platform-specific help information."
+}
 # Call appropriate function based on OS
 if [[ "$OS" == "Linux" ]]; then
   linux
 elif [[ "$OS" == "MINGW64_NT-10.0" ]]; then
   # This check is specific to Git Bash on Windows 10 (MINGW64_NT-10.0)
-  # You might need to adjust based on your Windows version
+  # You might need to adjust based on your Windows version MINGW64_NT-10.0-22631
+  termux_gitbash
+elif [[ "$OS" == "MINGW64_NT-10.0-22631"]]; then
   termux_gitbash
 else
   echo "Unsupported platform. Help information not available$OS"
